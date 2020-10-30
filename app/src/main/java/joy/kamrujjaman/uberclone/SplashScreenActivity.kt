2a54865d7812d2a20bf146f4b9fd39ef1f8cceeb
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2020-10-31 00:37:02 +0600

    location

--- app/src/main/java/joy/kamrujjaman/uberclone/SplashScreenActivity.kt
+++ app/src/main/java/joy/kamrujjaman/uberclone/SplashScreenActivity.kt
@@ -60,7 +60,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.activity_splash_screen)
 
         init()
--- app/src/main/java/joy/kamrujjaman/uberclone/ui/home/HomeFragment.kt
+++ app/src/main/java/joy/kamrujjaman/uberclone/ui/home/HomeFragment.kt
@@ -1,15 +1,23 @@
 package joy.kamrujjaman.uberclone.ui.home
 
+import android.Manifest
+import android.Manifest.permission
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,12 +25,29 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import joy.kamrujjaman.uberclone.R
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+
+    override fun onDestroy() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        super.onDestroy()
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,25 +56,121 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        init()
+
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         return root
     }
 
+    private fun init() {
+        locationRequest = LocationRequest()
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        locationRequest.setFastestInterval(3000)
+        locationRequest.interval = 5000
+        locationRequest.setSmallestDisplacement(10f)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+                if (ActivityCompat.checkSelfPermission(
+                        context!!,
+                        permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        context!!,
+                        permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    val newPos = LatLng(
+                        locationResult!!.lastLocation.latitude,
+                        locationResult!!.lastLocation.longitude
+                    )
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos, 18f))
+                    fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+                      fusedLocationProviderClient.requestLocationUpdates(
+                        locationRequest,
+                        locationCallback,
+                        Looper.myLooper()
+                    )
+                    true
+
+                }
+
+            }
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
-        mMap.uiSettings.isZoomControlsEnabled = true
+
+
+        Dexter.withContext(context)
+            .withPermission(permission.ACCESS_FINE_LOCATION)
+            .withListener(object : PermissionListener{
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    mMap.isMyLocationEnabled = true
+                    mMap.uiSettings.isMyLocationButtonEnabled =true
+                    mMap.setOnMyLocationClickListener {
+                        if (ActivityCompat.checkSelfPermission(
+                                context!!,
+                                permission.ACCESS_FINE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                context!!,
+                                permission.ACCESS_COARSE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+
+                            fusedLocationProviderClient.lastLocation
+                                .addOnFailureListener{e->
+                                    Toast.makeText(context,e.message,Toast.LENGTH_SHORT).show()
+
+                                }.addOnSuccessListener { location ->
+                                    val userLatLng = LatLng(location.latitude, location.longitude)
+                                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng,18f))
+                                }
+                            true
+                        }
+
+
+                        val locationButton = (mapFragment.view!!
+                            .findViewById<View>("1".toInt())
+                            .parent as View).findViewById<View>("2".toInt())
+                        val params = locationButton.layoutParams as RelativeLayout.LayoutParams
+                        params.addRule(RelativeLayout.ALIGN_TOP,0)
+                        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE)
+                        params.bottomMargin = 50
+
+                        }
+
+
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(context,"Permission"+ p0!!.permissionName+ "was Denied",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+            }).check();
+
         try {
-            val success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(context,R.raw.uber_maps_style))
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    context,
+                    R.raw.uber_maps_style
+                )
+            )
             if (!success)
-                Log.e("map","Style Parsing Error")
-        }catch (e:Resources.NotFoundException){
-            Log.e("map",e.message!!)
+                Log.e("map", "Style Parsing Error")
+        } catch (e: Resources.NotFoundException) {
+            Log.e("map", e.message!!)
         }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 }
