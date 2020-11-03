5c5d54c8e5f0dccc4aeceee13e1d424288b9ee90
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2020-11-03 23:17:48 +0600

    kaj kore na

--- app/src/main/java/joy/kamrujjaman/uberclone/Common.kt
+++ app/src/main/java/joy/kamrujjaman/uberclone/Common.kt
@@ -5,5 +5,6 @@ import joy.kamrujjaman.uberclone.model.DriverInfoModel
 object Common {
     var currentUser: DriverInfoModel? = null
     val DRIVER_INFO_REFERENCE: String = "DriverInfo"
+    val DRIVERS_LOCATION_REFERENCE: String = "DriverLocationInfo"
 
 }
--- app/src/main/java/joy/kamrujjaman/uberclone/ui/home/HomeFragment.kt
+++ app/src/main/java/joy/kamrujjaman/uberclone/ui/home/HomeFragment.kt
@@ -17,6 +17,8 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -25,12 +27,16 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
+import joy.kamrujjaman.uberclone.Common
 import joy.kamrujjaman.uberclone.R
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
@@ -42,12 +48,43 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var locationCallback: LocationCallback
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    //Online system
+    private lateinit var onlineRef:DatabaseReference
+    private lateinit var currentUserRef: DatabaseReference
+    private lateinit var driversLocationRef:DatabaseReference
+    private lateinit var geoFire:GeoFire
+
+    private val onlineValueEventListener = object :ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            if (snapshot.exists()){
+                currentUserRef.onDisconnect().removeValue()
+            }
+
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Snackbar.make(mapFragment.requireView()!!, error.message, Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+
+
 
     override fun onDestroy() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        geoFire.removeLocation(FirebaseAuth.getInstance().currentUser!!.uid)
+        onlineRef.removeEventListener(onlineValueEventListener)
         super.onDestroy()
     }
 
+    override fun onResume() {
+        super.onResume()
+        registerOnlineSystem()
+    }
+
+    private fun registerOnlineSystem() {
+        onlineRef.addValueEventListener(onlineValueEventListener)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,40 +101,63 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     }
 
     private fun init() {
+
+        onlineRef = FirebaseDatabase.getInstance().getReference().child(".info/connected")
+        driversLocationRef = FirebaseDatabase.getInstance().getReference(Common.DRIVERS_LOCATION_REFERENCE)
+        currentUserRef = FirebaseDatabase.getInstance().getReference(Common.DRIVERS_LOCATION_REFERENCE).child(
+            FirebaseAuth.getInstance().currentUser!!.uid
+        )
+        geoFire = GeoFire(driversLocationRef)
+        registerOnlineSystem()
+
+
         locationRequest = LocationRequest()
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
         locationRequest.setFastestInterval(3000)
         locationRequest.interval = 5000
         locationRequest.setSmallestDisplacement(10f)
+
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult?) {
                 super.onLocationResult(locationResult)
-                if (ActivityCompat.checkSelfPermission(
-                        context!!,
-                        permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        context!!,
-                        permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
 
-                    val newPos = LatLng(
-                        locationResult!!.lastLocation.latitude,
-                        locationResult!!.lastLocation.longitude
-                    )
+
+                    val newPos = LatLng(locationResult!!.lastLocation.latitude, locationResult!!.lastLocation.longitude)
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos, 18f))
-                    fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
-                      fusedLocationProviderClient.requestLocationUpdates(
-                        locationRequest,
-                        locationCallback,
-                        Looper.myLooper()
-                    )
-                    true
 
+                //update location
+                geoFire.setLocation(
+                    FirebaseAuth.getInstance().currentUser!!.uid,
+                    GeoLocation(locationResult.lastLocation.latitude,locationResult.lastLocation.longitude)
+
+                ){key: String?, error: DatabaseError? ->
+                    if (error!=null)
+                        Snackbar.make(mapFragment.requireView(),error.message,Snackbar.LENGTH_LONG).show()
+                    else
+                        Snackbar.make(mapFragment.requireView(),"You are Online",Snackbar.LENGTH_LONG).show()
                 }
 
+
             }
+
         }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+        if (ActivityCompat.checkSelfPermission(
+                context!!,
+                permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context!!,
+                permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.myLooper()
+            )
+            return
+        }
+
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
